aab0167bc37f104e370c7a8944e57da4ff5778f0
Vitor Miranda <vitortcmout@gmail.com>
2020-01-29 22:13:18 -0300

    Initial commit

---
+++ alurapic/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <h1>{{titulo}}</h1> 
+    <ul>
+      <li v-for="foto of fotos">
+        <img :src="foto.url" :alt="foto.titulo">
+      </li>  
+    </ul>   
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  data () {
+    return {
+      titulo: 'Alura- Curso de VueJs',
+      fotos: [{
+        url:'assets/logo.png',
+        titulo:'Vue',
+      },
+      {
+        url:'assets/logo.png',
+        titulo:'Vue',
+      }
+      
+      ],
+    }
+  }
+}
+</script>
+
+<style>
+</style>
